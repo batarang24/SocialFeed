64fef3fb192a595b3fce28749df9653e92d28d3a
Naren <narenvasu2003@gmail.com>
2025-01-19 18:33:19 +0530

    local rub

--- backend/db.js
+++ backend/db.js
@@ -2,14 +2,11 @@ const mysql = require('mysql2');
 
 // Create a connection pool
 const pool = mysql.createPool({
-  host: 'sql107.infinityfree.com',      // Host from InfinityFree
-  user: 'if0_38135226',               // Your MySQL username
-  password: 'HEZlLgcxRFFI',           // Your MySQL password
-  database: 'if0_38135226_social_feed',      // Your database name
-  port: 3306,                          // Default MySQL port
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  host: 'localhost',      // Host from InfinityFree
+  user: 'root',               // Your MySQL username
+  password: '1234',           // Your MySQL password
+  database: 'social_feed',      // Your database name
+ 
 });
 
 // Export the pool to be used in other parts of the app
