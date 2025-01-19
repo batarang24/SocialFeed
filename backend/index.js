2f198949d4ae9f24d17ad8f0c26984f6d1f9221c
Naren <narenvasu2003@gmail.com>
2025-01-19 16:21:56 +0530

    Updating the port in server

--- backend/index.js
+++ backend/index.js
@@ -109,6 +109,7 @@ app.post('/api/posts/:postId/comment', async (req, res) => {
 });
 
 // Start the server
-app.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('Server running ');
 });
